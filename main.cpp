40c70f9a9fe3c605835d392cd7847a5d429a2b78
Alec Vitalievich <alecvitalievich@Alecs-MacBook-Pro.local>
2025-05-15 22:38:46 +0930

    changed resolution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Player.h"
 
 int main(){
-    Game game(1920,1080,"Sunflower Journey: The Quest For Sun",60,1);
+    Game game(1800,1020,"Sunflower Journey: The Quest For Sun",60,1);
     game.update();
     return 0;
-}
+} 
